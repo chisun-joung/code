e91c414641672cb02fe2afb9bddc116b7bdf0aca
chisun <chisun.joung@lge.com>
2023-06-25 23:41:52 +0900

    7.8

--- src/allocation/adapters/repository.py
+++ src/allocation/adapters/repository.py
@@ -20,4 +20,8 @@ class SqlAlchemyRepository(AbstractRepository):
         self.session.add(product)
 
     def get(self, sku):
-        return self.session.query(model.Product).filter_by(sku=sku).first()
+        return (
+            self.session.query(model.Product).filter_by(sku=sku)
+            # .with_for_update()
+            .first()
+        )
