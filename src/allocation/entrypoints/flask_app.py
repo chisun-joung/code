01c2090b77d5d89e5847f1ca36dc3901ef5883b5
chisun <chisun.joung@lge.com>
2023-06-25 16:59:50 +0900

    end chapter6

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from allocation import config
 from allocation.domain import model
 from allocation.adapters import orm, repository
-from allocation.service_layer import services
+from allocation.service_layer import services, unit_of_work
 
 orm.start_mappers()
 get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
@@ -15,8 +15,6 @@ app = Flask(__name__)
 
 @app.route("/add_batch", methods=["POST"])
 def add_batch():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
     eta = request.json["eta"]
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
@@ -25,23 +23,19 @@ def add_batch():
         request.json["sku"],
         request.json["qty"],
         eta,
-        repo,
-        session,
+        unit_of_work.SqlAlchemyUnitOfWork(),
     )
     return "OK", 201
 
 
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
     try:
         batchref = services.allocate(
             request.json["orderid"],
             request.json["sku"],
             request.json["qty"],
-            repo,
-            session,
+            unit_of_work.SqlAlchemyUnitOfWork(),
         )
     except (model.OutOfStock, services.InvalidSku) as e:
         return {"message": str(e)}, 400
