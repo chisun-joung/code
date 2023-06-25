e7aa690ddc58986d4ecae6c4975e0bccb14d0373
chisun <chisun.joung@lge.com>
2023-06-25 14:42:20 +0900

    ready to 6.2

--- src/allocation/service_layer/services.py
+++ src/allocation/service_layer/services.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 from typing import Optional
 from datetime import date
 
-from domain import model
-from domain.model import OrderLine
-from adapters.repository import AbstractRepository
+from allocation.domain import model
+from allocation.domain.model import OrderLine
+from allocation.adapters.repository import AbstractRepository
 
 
 class InvalidSku(Exception):
@@ -16,16 +16,19 @@ def is_valid_sku(sku, batches):
 
 
 def add_batch(
-    ref: str, sku: str, qty: int, eta: Optional[date],
-    repo: AbstractRepository, session,
+    ref: str,
+    sku: str,
+    qty: int,
+    eta: Optional[date],
+    repo: AbstractRepository,
+    session,
 ) -> None:
     repo.add(model.Batch(ref, sku, qty, eta))
     session.commit()
 
 
 def allocate(
-    orderid: str, sku: str, qty: int,
-    repo: AbstractRepository, session
+    orderid: str, sku: str, qty: int, repo: AbstractRepository, session
 ) -> str:
     line = OrderLine(orderid, sku, qty)
     batches = repo.list()
