02255f570bfba7e0eed4e7064c344729e4c35dfc
chisun <chisun.joung@lge.com>
2023-06-26 18:22:08 +0900

    test

--- src/allocation/service_layer/unit_of_work.py
+++ src/allocation/service_layer/unit_of_work.py
@@ -38,7 +38,7 @@ class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
     def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
         self.session_factory = session_factory
 
-    def __enter__(self) -> SqlAlchemyUnitOfWork:
+    def __enter__(self):
         self.session = self.session_factory()  # type: Session
         self.products = repository.SqlAlchemyRepository(self.session)
         return super().__enter__()
